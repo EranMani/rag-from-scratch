60e00628d308211fc709a71e19c826cca8720579
EranMani <eranmanim@gmail.com>
2026-05-04 20:48:36 +0300

    refactor: add enum class for chunk strategy and create mapped dict for strategy list actions

--- rag/pipeline/chunker.py
+++ rag/pipeline/chunker.py
@@ -1,19 +1,33 @@
 from dataclasses import dataclass
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from src.app.core.logging_config import logger
+from app.core.logging_config import logger
+from enum import StrEnum
+
+
+class ChunkStrategy(StrEnum):
+    FIXED = "fixed"
+    SENTENCE = "sentence"
+    RECURSIVE = "recursive"
 
 
 @dataclass
 class ChunkConfig:
     chunk_size: int = 500
     chunk_overlap: int = 100
-    strategy: str = "recursive" # fixed | recursive | sentence
+    strategy: ChunkStrategy = ChunkStrategy.RECURSIVE
+
+SEPARATORS: dict[ChunkStrategy, list[str]] = {
+    ChunkStrategy.FIXED: [],
+    ChunkStrategy.SENTENCE: ["\n\n", "\n", ". "],
+    ChunkStrategy.RECURSIVE: ["\n\n", "\n", ". ", " ", ""],
+}
 
 
-def chunk_documents(docs: list[Document], config: ChunkConfig) -> list[Document]:
+def chunk_documents(docs: list[Document], config: ChunkConfig = ChunkConfig()) -> list[Document]:
     """
     Split a list of Langchain documents into smaller chunks
+    Return a list of chunks (Document objects)
 
     NOTE: Without chunking, a 500 page document is embedded as a single vector,
           losing all precision.
@@ -30,33 +44,14 @@ def chunk_documents(docs: list[Document], config: ChunkConfig) -> list[Document]
         }
     )
 
-    if config.strategy == "fixed":
-        # Split every N characters - fast but can break mid-sentence
-        splitter = RecursiveCharacterTextSplitter(
-            chunk_size=config.chunk_size,
-            chunk_overlap=config.chunk_overlap,
-            separators=[], # no preferred separators, pure size
-        )
+    # Create the splitter engine
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=config.chunk_size,
+        chunk_overlap=config.chunk_overlap,
+        separators=SEPARATORS[config.strategy]
+    )
 
-    elif config.strategy == "sentence":
-        # Split only on sentence/paragraph boundries
-        splitter = RecursiveCharacterTextSplitter(
-            chunk_size=config.chunk_size,
-            chunk_overlap=config.chunk_overlap,
-            separators=["\n\n", "\n", ". "]
-        )
-
-    elif config.strategy == "recursive":
-        # Try paragraph -> sentence -> word -> character splits in order
-        splitter = RecursiveCharacterTextSplitter(
-            chunk_size=config.chunk_size,
-            chunk_overlap=config.chunk_overlap,
-            separators=["\n\n", "\n", ". ", " ", ""]
-        )
-
-    else:
-        raise ValueError(f"Unkown chunking strategy: {config.strategy}")
-
+    # Build chunks from the given documents
     chunks = splitter.split_documents(docs)
 
     logger.info(
